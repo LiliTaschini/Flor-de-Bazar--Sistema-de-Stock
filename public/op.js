71002cc4fdbf2624a5b7305c84dae7fca4ff5546
LiliTaschini <liliantaschini@gmail.com>
2023-01-19 01:10:11 -0300

    'json'

--- public/op.js
+++ public/op.js
@@ -2,9 +2,11 @@ const app = Vue.createApp({
 
     data() {
       return {
-        page: "inicio",
+        /* páginas */
+        page: "productos",
         pagePorcentaje: 'aumento',
         pageInicio: 'iniciar',
+        /*  */
         producto: '', 
         código: '', 
         categoria: '',   
@@ -13,7 +15,61 @@ const app = Vue.createApp({
         precio: '',  
         descuento: '', 
         aumento: '', 
-        nuevaCategoria: '', 
+        nuevaCategoria: '',
+        /* login */
+        usuario: null, 
+        contraseña: '',
+        inicioSesion: true,  
+      }
+    },
+    methods:{
+      iniciarSesion(){
+        if(this.usuario!='' && this.contraseña!=''){
+          firebase.auth().signInWithEmailAndPassword(this.usuario, this.contraseña)
+          .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            this.usuario = user;
+            this.contraseña = '';
+            this.page = 'productos'
+            this.inicioSesion = true;
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'top-end',
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+              }
+            })
+            Toast.fire({
+              icon: 'success',
+              title: 'Inicio Correcto'
+            })
+            // ...
+          })
+          .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+          });  
+      }else{
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        Toast.fire({
+          icon: 'error',
+          title: 'Por favor, ingrese con las credenciales correctas para iniciar el sistema'
+        })
       }
     }
-  }).mount('#app')
+  }}).mount('#app')
